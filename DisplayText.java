ca9ebe6d3cfd60637abff075adecc0e6e3ef7cf0
Colby Ruane <125488077+ColbyRuane@users.noreply.github.com>
2023-02-15 13:09:25 -0600

    Update DisplayText.java

--- DisplayText.java
+++ DisplayText.java
@@ -2,6 +2,6 @@ class DisplayText
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Okay. $19 Dollar Fortnite card. Who wants it? And yes, I'm giving it away. Remember, share share share! And trolls, don't get blocked.");
+		System.out.println("Okay. $19 Dollar Fortnite card. Who wants it? And yes, I'm giving it away.\nRemember, share share share! And trolls, don't get blocked.");
 	}
 }
